7484823df6c2e16e58556b29e55e63fc067e9c4d
Shakil Mahmud Arafat <ahmed.arafat666@gmail.com>
2023-01-29 04:42:26 +0600

    design change initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import Construction from "../components/Construction";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
@@ -6,6 +7,18 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZFHK8EKX3T"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZFHK8EKX3T');`}
+      </Script>
       <Nav />
       <Component {...pageProps} />
       <Construction />
